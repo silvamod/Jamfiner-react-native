46ab2b063fcb116383ba126aca805b0dcb512e26
Omer Leibu <omer.leibu@gmail.com>
2022-02-03 12:21:05 +0200

    camera added

--- Components/Profile/Profile.jsx
+++ Components/Profile/Profile.jsx
@@ -3,16 +3,20 @@ import React from 'react';
 import { ProfileHeader } from "@freakycoder/react-native-header-view";
 import AwesomeButton from "react-native-really-awesome-button";
 import Icon from 'react-native-vector-icons/Feather';
+import Popup from '../Camera/Popup';
 
 export default function Profile(props) {
   return (
     <>
 
-
     <View style={styles.container}>
     <Image style={styles.image} source={{uri:"http://jillchloekenny.com/koken/storage/cache/images/000/027/Indian-Man,medium_large.2x.1428057054.jpg", width:'100%',height:200 }}/>
       <View>
-        <AwesomeButton width={50} style={styles.cambtn}>
+        <AwesomeButton 
+        width={50} 
+        style={styles.cambtn}
+        onPress={()=>fireCameraPopUp()}
+        >
         <Icon
          name="camera"
          size={40}
@@ -21,6 +25,7 @@ export default function Profile(props) {
       </View>
       <Text style={styles.name}>Your Name</Text>
       <Text style={styles.bio}>Bio</Text>
+      <Popup/>
     </View>
     </>
   );
