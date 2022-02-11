86630c0de2c49be9571a2997ddafb8396c7bb117
Mahdius <silvamod@gmail.com>
2022-02-11 16:30:15 +0200

    stam chat

--- Components/Chat/Chat.jsx
+++ Components/Chat/Chat.jsx
@@ -1,13 +1,45 @@
 import { View, Text ,StyleSheet} from 'react-native';
-import React from 'react';
+import React, { useState, useCallback, useEffect } from 'react'
+import { GiftedChat } from 'react-native-gifted-chat'
+
+
 
 export default function Chat() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+    ])
+  }, [])
+
+  const onSend = useCallback((messages = []) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
+  }, [])
+
+
+
   return (
-    <View style={styles.container}>
-      <Text>Chat !!!</Text>
-    </View>
+    <GiftedChat
+    messages={messages}
+    showAvatarForEveryMessage={true}
+    onSend={messages => onSend(messages)}
+    user={{
+      _id: 1,
+    }}
+  />
   );
 }
+
 const styles = StyleSheet.create({
     container: {
        flex: 1,
