a9f56e0d63f91f53430ad0c5a3607e54d8d6f9f3
Omer Leibu <omer.leibu@gmail.com>
2022-07-17 20:06:15 +0300

    stam

--- Components/Login/Button.tsx
+++ Components/Login/Button.tsx
@@ -1,25 +1,25 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { View, Text } from "react-native";
+import React from "react";
 import AwesomeButton from "react-native-really-awesome-button";
 
 export default function Button(props) {
   return (
     <View>
       <AwesomeButton
-      onPress={() => {
-        /** Do Something **/
-        props.onP()
-      }}
-      textSize={23}
-      backgroundColor={'#1a1a1a'}
-      width={props.size}
-      backgroundDarker={'#191414'}
-      textColor={'#fff'}
-      style={{marginTop:10,alignSelf:'stretch'}}
-      
-    >
-      {props.label}
-    </AwesomeButton>
+        onPress={() => {
+          /** Do Something **/
+          props.onP();
+        }}
+        borderRadius={20}
+        textSize={23}
+        backgroundColor={"#1a1a1a"}
+        width={props.size}
+        backgroundDarker={"#191414"}
+        textColor={"#fff"}
+        style={{ marginTop: 10, alignSelf: "stretch" }}
+      >
+        {props.label}
+      </AwesomeButton>
     </View>
   );
 }
