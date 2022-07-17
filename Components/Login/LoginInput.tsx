3ee827d461a44a1979f59a6ac2dbbc06d4282eee
Omer Leibu <omer.leibu@gmail.com>
2022-07-17 20:15:09 +0300

    stam

--- Components/Login/LoginInput.tsx
+++ Components/Login/LoginInput.tsx
@@ -1,38 +1,35 @@
-import { View, Text } from 'react-native';
-import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
-import { Hideo } from 'react-native-textinput-effects';
+import { View, Text } from "react-native";
+import FontAwesomeIcon from "react-native-vector-icons/FontAwesome";
+import { Hideo } from "react-native-textinput-effects";
 import React from "react";
 import { SafeAreaView, StyleSheet, TextInput } from "react-native";
 
-export default function LoginInput(props) {  
-
+export default function LoginInput(props) {
   return (
-   <>
+    <>
       <TextInput
         style={styles.input}
         placeholder={props.lable}
-        onChangeText={(text)=>{
-          props.set(text)
-          console.log(text)
-          }}
-          secureTextEntry={props.pass}
+        onChangeText={(text) => {
+          props.set(text);
+          console.log(text);
+        }}
+        secureTextEntry={props.pass}
       />
-
     </>
-      );
+  );
 }
-
 
 const styles = StyleSheet.create({
   input: {
-    direction:'ltr',
+    direction: "ltr",
     height: 55,
     margin: 12,
     borderWidth: 2,
     padding: 10,
-    borderRadius:5,
-    alignSelf:'stretch',
-    marginLeft:60,
-    marginRight:60
+    borderRadius: 5,
+    alignSelf: "stretch",
+    marginLeft: 60,
+    marginRight: 60,
   },
 });
